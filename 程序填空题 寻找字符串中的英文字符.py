aec2f53c76208829908242dc020170ead1ddf691
Zhu-Aemon <63136983+Zhu-Aemon@users.noreply.github.com>
2021-10-24 19:38:00 +0800

    Delete 程序填空题 寻找字符串中的英文字符.py

--- 程序填空题 寻找字符串中的英文字符.py
+++
@@ -1,9 +0,0 @@
-a = input()
-li = []
-for i in range(len(a)):  # 遍历字符串
-    if ord('a') <= ord(a[i]) <= ord('z') or ord('A') <= ord(a[i]) <= ord('Z'):  # 判断是否是英语字母
-        ch = a[i].lower()  # 英文字母取小写
-        if ch not in li:  # 判断字母是否在列表中
-            li.append(ch)  # 将符合新的英文字母追加到列表
-for ch in li:
-    print(ch, end='')  # 字符连续输出，不空格，不换行
